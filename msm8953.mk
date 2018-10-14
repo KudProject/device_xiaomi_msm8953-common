f81dbf394122f9a7cd92564c4fba95922acf5194
soshiweekly <1376106928@qq.com>
2018-10-14 13:46:12 +0800

    msm8953-common: Add Mi 5X support
    Change-Id: I5ce9fc3b19f2aaf5aa66965e1fc2425aa15371ac
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    [kras: Clean up non-existent OSS packages]
    Signed-off-by: Albert I <kras@raphielgang.org>

--- msm8953.mk
+++ msm8953.mk
@@ -88,6 +88,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
+    libtinycompress \
     tinymix
 
 # Audio configuration
@@ -321,7 +322,7 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
