e3a5e544b54ae2f95cf084d87324f5fdd0118739
Albert I <kras@raphielgang.org>
2019-10-28 03:28:19 +0800

    msm8953-common: Xenon-ify
    Signed-off-by: Albert I <kras@raphielgang.org>

--- extract-files.sh
+++ extract-files.sh
@@ -22,9 +22,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+XENONHD_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$XENONHD_ROOT"/vendor/xenonhd/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -53,25 +53,25 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$XENONHD_ROOT" true "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+    setup_vendor "$DEVICE" "$VENDOR" "$XENONHD_ROOT" false "$CLEAN_VENDOR"
 
     extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" "$SECTION"
 fi
 
 if [ "$DEVICE" = "markw" ] || [ "$DEVICE" = "mido" ] || [ "$DEVICE" = "tiffany" ]; then
     # Hax for cam configs
-    CAMERA2_SENSOR_MODULES="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
+    CAMERA2_SENSOR_MODULES="$XENONHD_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
     sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
 
     # Hax for cam data location
-    MM_QCAMERA_DAEMON="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/bin/mm-qcamera-daemon
-    CAMERA_LIB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib
+    MM_QCAMERA_DAEMON="$XENONHD_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/bin/mm-qcamera-daemon
+    CAMERA_LIB_ROOT="$XENONHD_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib
     sed -i "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" "$MM_QCAMERA_DAEMON"
     for CAMERA_LIB in libmmcamera2_cpp_module.so libmmcamera2_dcrf.so libmmcamera2_iface_modules.so libmmcamera2_imglib_modules.so libmmcamera2_mct.so libmmcamera2_pproc_modules.so libmmcamera2_q3a_core.so libmmcamera2_sensor_modules.so libmmcamera2_stats_algorithm.so libmmcamera2_stats_modules.so libmmcamera_dbg.so libmmcamera_imglib.so libmmcamera_pdafcamif.so libmmcamera_pdaf.so libmmcamera_tintless_algo.so libmmcamera_tintless_bg_pca_algo.so libmmcamera_tuning.so; do
         sed -i "s|/data/misc/camera/|/data/vendor/qcam/|g" "$CAMERA_LIB_ROOT"/$CAMERA_LIB
